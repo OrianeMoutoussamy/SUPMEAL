f3aa918d4d224030aeaf7ecc23a8ac74f3051947
moutoussamy-oriane <moutoussamyoriane@gmail.com>
2026-09-03 11:53:10 +0200

    Front docker

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to React</h1>
+                <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            </header>
+        </div>
+    );
+}
